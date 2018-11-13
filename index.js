245aecb106e3ab11a6f4fd41c9b4bfbb8bb3be1e
Naenae L <oneprincesslea@gmail.com>
2018-11-13 00:13:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,13 +10,15 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 };
-console.log(yourAnimal()); 
+console.log(yourAnimal());
 
 function add2(n) {
   const two = 2
   n = 5;
   return n + two;
 }
+console.log(add2()); 
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
